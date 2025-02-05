42dfea9ff6e31afb96acbfe1a5d95cf7d4055b2c
kade <11649754+go-kade@user.noreply.gitee.com>
2025-02-05 18:28:16 +0800

    additional unMarshal method

--- tools/format/yaml.go
+++ tools/format/yaml.go
@@ -1,6 +1,11 @@
 package format
 
-import "sigs.k8s.io/yaml"
+import (
+	"encoding/json"
+
+	"sigs.k8s.io/yaml"
+)
+
 
 func MustToYaml(v any) string {
 	b, err := yaml.Marshal(v)
@@ -8,4 +13,10 @@ func MustToYaml(v any) string {
 		panic(err)
 	}
 	return string(b)
+}
+
+// This function takes in a byte slice and an empty interface and returns an error
+func Unmarshal(data []byte, v any) error {
+	// Unmarshal the byte slice into the empty interface
+	return json.Unmarshal(data, v)
 }
